a4a5994ab0bd492607ca4a7ddde06c8018d22e8c
hilaklein <kleinhi@post.bgu.ac.il>
2020-12-07 21:02:09 +0200

    Initial commit

---
+++ SPL211/src/test/java/bgu/spl/mics/MessageBusImplTest.java
@@ -0,0 +1,34 @@
+package bgu.spl.mics;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageBusImplTest {
+    MessageBusImpl messageBus;
+    @BeforeEach
+    void setUp() {
+        messageBus = new MessageBusImpl();
+    }
+
+    @Test
+    void subscribeEvent() {
+    }
+
+    @Test
+    void subscribeBroadcast() {
+    }
+
+    @Test
+    void complete() {
+    }
+
+    @Test
+    void sendBroadcast() {
+    }
+
+    @Test
+    void sendEvent() {
+    }
+}
